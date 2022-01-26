9a74b34a21b0e22da470c0c1620744a798ce1eef
wax-on <hugo.johansson@medieinstitutet.se>
2022-01-26 21:20:45 +0100

    First commit

---
+++ assets/js/topScroll.js
@@ -0,0 +1,21 @@
+const mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 250 ||
+    document.documentElement.scrollTop > 250
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
